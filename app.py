4feb7688659beb5db3dc7add70db4890e2f2819a
אלון אלימלך <aloni12345@gmail.com>
2025-01-12 21:30:14 +0200

    added kubernetes manifests for deploy, added support for swagger docs in Airgap environment

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from dns.routes import router as dns_router
 
 from common.logging import setup_logging
 from common.metrics import setup_metrics
+from common.swagger import swagger_initialize
 
 # Initialize logging and metrics
 setup_logging()
 
 # Initialize FastAPI app
-app = FastAPI()
+app = FastAPI(docs_url=None)
+
+swagger_initialize(app)
 
 # Include DNS routes
 app.include_router(dns_router, prefix="/dns", tags=["DNS CRUD Operations"])
---
+++ common/swagger.py
@@ -0,0 +1,24 @@
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
+from fastapi.staticfiles import StaticFiles
+
+
+def swagger_initialize(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+
+    @app.get("/docs", include_in_schema=False)
+    async def custom_swagger_ui_html():
+        return get_swagger_ui_html(
+            openapi_url=app.openapi_url,
+            title="DNS API",
+            swagger_js_url="/static/swagger-ui-bundle.js",
+            swagger_css_url="/static/swagger-ui.css",
+            swagger_favicon_url="/static/favicon.png",
+        )
+    
+    @app.get("/openapi.json", include_in_schema=False)
+    async def get_open_api():
+        return get_openapi(title="My API", version="1.0.0", routes=app.routes)
+    
