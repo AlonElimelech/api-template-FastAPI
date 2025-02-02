f29b06cdaa79ee1a6167ff5c8d6ca7465908f6b3
אלון אלימלך <aloni12345@gmail.com>
2025-02-02 22:01:26 +0200

    added field validaor to validators sections

--- dns/schemas.py
+++ dns/schemas.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, field_validator
 import re
 
 class DNSRecordCreate(BaseModel):
     record_name: str = Field(..., max_length=15)
     ip: str
 
-    @validator('ip')
+    @field_validator('ip')
     def validate_ip(cls, v):
         ip_pattern = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
         if not re.match(ip_pattern, v):
@@ -21,7 +21,7 @@ class DNSRecordResponse(BaseModel):
 class DNSRecordUpdate(BaseModel):
     ip: str
 
-    @validator('ip')
+    @field_validator('ip')
     def validate_ip(cls, v):
         ip_pattern = r'^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
         if not re.match(ip_pattern, v):
