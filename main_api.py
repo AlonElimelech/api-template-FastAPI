70ad05713f04b6e2745ebf8282934946f5039cac
אלון אלימלך <aloni12345@gmail.com>
2025-02-02 21:58:33 +0200

    changed app.py

--- main_api.py
+++ main_api.py
@@ -1,28 +1,26 @@
 from fastapi import FastAPI
 from dns.routes import router
-from dns.settings import Settings
+from dns.settings import API_TITLE, API_DESCRIPTION, API_VERSION, API_PREFIX, API_TAGS
 
-from common.logging import setup_logging
 from common.metrics import setup_metrics
 from common.swagger import swagger_initialize
 
-# Initialize settings
-settings = Settings()
+
 
 # Initialize logging and metrics
-setup_logging()
+#setup_logging()
 
 # Initialize FastAPI app
 app = FastAPI(docs_url=None,
-            title=settings.API_TITLE,
-            description=settings.API_DESCRIPTION,
-            version=settings.API_VERSION)
+            title=API_TITLE,
+            description=API_DESCRIPTION,
+            version=API_VERSION)
 
 
 swagger_initialize(app)
 
 # Include DNS routes
-app.include_router(router, prefix=settings.API_PREFIX, tags=[settings.API_TAGS])
+app.include_router(router, prefix=API_PREFIX, tags=[API_TAGS])
 
 # Setup metrics
 setup_metrics(app)
