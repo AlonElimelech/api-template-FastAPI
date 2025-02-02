5b56071b7d4c7fb2ecfefb5d9b60dee2770b9d6a
אלון אלימלך <aloni12345@gmail.com>
2025-02-02 22:02:54 +0200

    added changes in routes that become the high level function trigger

--- dns/operation_dns.py
+++ dns/operation_dns.py
@@ -1,23 +1,22 @@
 # Description: This file contains the functions to create and delete DNS records.
 import common.awx_client as awx_client
 # The create_dns function creates a DNS record with the provided record name and IP address.
-async def create_dns(record_name: str, ip: str) -> str:
+def create_dns(record_name: str, ip: str) -> int:
     print("create")
-    response = awx_client.launch_job()
-
-    return response.status_code
+    #response = awx_client.launch_job()
+    return 200
 
 # The delete_dns function deletes a DNS record with the provided record name.
-async def delete_dns(record_name: str) -> str:
+def delete_dns(record_name: str) -> str:
     print("delete")
-    return "DNS record deleted successfully"
+    return 200
 
-async def update_dns(record_name: str, ip: str) -> str:
+def update_dns(record_name: str, ip: str) -> str:
     """Update DNS record with new IP"""
     print(f"update dns record: {record_name} with ip: {ip}")
     return "DNS record updated successfully"
 
-async def get_dns(record_name: str) -> str:
+def get_dns(record_name: str) -> str:
     """Get DNS record IP"""
     print(f"get dns record: {record_name}")
-    return "192.168.1.1"  # Simulated IP address
+    return 200  # Simulated IP address
--- dns/routes.py
+++ dns/routes.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from dns.schemas import DNSRecordCreate, DNSRecordResponse, DNSRecordUpdate
-from dns.service import create_dns_record, delete_dns_record, update_dns_record, get_dns_record
+from common.logging import logger
+from common.metrics import dns_create_counter, dns_delete_counter, increment_create_counter, increment_delete_counter, increment_update_counter, increment_failure_counter
+from dns.operation_dns import create_dns, delete_dns, update_dns, get_dns 
 
 router = APIRouter()
 
@@ -9,29 +11,61 @@ router = APIRouter()
     summary="Create DNS Record",
     description="Creates a new DNS record with the specified Name and IP address"
 )
-async def create_dns(record: DNSRecordCreate):
-    return await create_dns_record(record)
+def create(record: DNSRecordCreate):
+    try:
+        logger.info(f"Creating DNS record: {record.record_name}")
+        # Here is the implementation for creating DNS record by Domain services Team
+        response = create_dns(record.record_name, record.ip)
+        increment_create_counter(record.record_name, record.ip)
+        return DNSRecordResponse(record_name=record.record_name, status_code=response, status= "in progress")
+    except Exception as e:
+        logger.error(f"Failed to create DNS record: {str(e)}")
+        increment_failure_counter("create", record.record_name)
+        raise HTTPException(status_code=404, detail=str(e))
 
 @router.delete("/{record_name}",
     response_model=DNSRecordResponse,
     summary="Delete DNS Record",
     description="Deletes an existing DNS record by its name"
 )
-async def delete_dns(record_name: str):
-    return await delete_dns_record(record_name)
-
+def delete(record_name: str):
+    try:
+        logger.info(f"Deleting DNS record: {record_name}")
+        response = delete_dns(record_name)
+        increment_delete_counter(record_name)
+        return DNSRecordResponse(record_name=record_name,status_code=response, status= "deleted")
+    except Exception as e:
+        logger.error(f"Failed to delete DNS record: {str(e)}")
+        increment_failure_counter("delete", record_name)
+        raise HTTPException(status_code=404, detail=str(e))
+    
 @router.put("/{record_name}", 
     response_model=DNSRecordResponse,
     summary="Update DNS Record",
     description="Updates the IP address of an existing DNS record"
 )
-async def update_dns(record_name: str, record: DNSRecordUpdate):
-    return await update_dns_record(record_name, record)
+def update(record_name: str, record: DNSRecordUpdate):
+    try:
+        logger.info(f"Updating DNS record: {record_name} with IP: {record.ip}")
+        response =  update_dns(record_name, record.ip)
+        increment_update_counter(record_name)
+        return DNSRecordResponse(record_name=record_name, status= "updated")
+    except Exception as e:
+        logger.error(f"Failed to update DNS record: {str(e)}")
+        increment_failure_counter("update", record_name)
+        raise HTTPException(status_code=404, detail=str(e))
 
 @router.get("/{record_name}", 
     response_model=DNSRecordResponse,
     summary="Get DNS Record",
     description="Retrieves the details of a DNS record by its name"
 )
-async def get_dns(record_name: str):
-    return await get_dns_record(record_name)
+def get(record_name: str):
+    try:
+        logger.info(f"Getting DNS record: {record_name}")
+        response = get_dns(record_name)
+        return DNSRecordResponse(record_name=record_name, status_code=response ,status= "fetched")
+    except Exception as e:
+        logger.error(f"Failed to get DNS record: {str(e)}")
+        increment_failure_counter("get", record_name)
+        raise HTTPException(status_code=404, detail=str(e))
