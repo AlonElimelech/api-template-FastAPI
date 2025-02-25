a9e4c93317f23a681490ab4c501e6724592df130
אלון אלימלך <aloni12345@gmail.com>
2025-02-25 23:12:54 +0200

    added label on uptime metrics

--- common/metrics.py
+++ common/metrics.py
@@ -4,18 +4,19 @@ from fastapi import Request, FastAPI
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 # Uptime metric
-uptime_metrics = Gauge("uptime_seconds", "Uptime in seconds")
+uptime_metrics = Gauge("uptime_seconds", "Uptime in seconds", ["app_name"])
 start_time = time.time()
 
-def setup_metrics(app: FastAPI):
-    app.add_middleware(PrometheusMiddleware, app_name="dns")
-    app.add_route("/metrics", custom_metrics)
+def setup_metrics(app: FastAPI, resource_name: str):
+    app.add_middleware(PrometheusMiddleware, app_name=resource_name)
+    app.add_route("/metrics", custom_metrics(resource_name))
 
-
-def custom_metrics(request: Request):
-    """Expose metrics with added uptime tracking."""
-    uptime_metrics.set(time.time() - start_time)
-    return handle_metrics(request)
+def custom_metrics(resource_name: str):
+    def metrics_handler(request: Request):
+        """Expose metrics with added uptime tracking."""
+        uptime_metrics.labels(app_name=resource_name).set(time.time() - start_time)
+        return handle_metrics(request)
+    return metrics_handler
 
 
 
