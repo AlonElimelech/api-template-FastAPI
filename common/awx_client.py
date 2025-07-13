1e35870a32e0432f166e1af9bd4e2f471797f44c
אלון אלימלך <aloni12345@gmail.com>
2025-07-13 22:11:40 +0300

    refactor(awx_client.py): remove BackgroundTask usage

--- common/awx_client.py
+++ common/awx_client.py
@@ -1,17 +1,21 @@
 import requests
 import asyncio
-from fastapi import BackgroundTasks
 
 
 
-def launch_job(base_url: str,job_template_id: int, headers: str, extra_vars: dict, callback: str ,background_tasks: BackgroundTasks):
+def launch_job(base_url: str,job_template_id: int, headers: str, extra_vars: dict):
     url = f"{base_url}/api/v2/job_templates/{job_template_id}/launch/"
     response = requests.post(url, headers=headers, json={"extra_vars": extra_vars})
 
-    if response.status_code != 201:
+    if response.status_code != 200:
         raise Exception(f"Failed to launch job: {response.status_code} - {response.text}")
-    job_id = response.json().get("job")
-    background_tasks.add_task(polling_awx_job_status, base_url, job_id, headers, callback)
+    
+    json_response = response.json()
+    
+    return { "status": "success",
+            "job_id": response.json()["job"],
+            "return_code": response.status_code,
+            "stdout": base_url + json_response["url"] }
 
 
     return response.json()
